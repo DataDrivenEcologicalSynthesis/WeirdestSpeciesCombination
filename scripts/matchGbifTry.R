1f8ba6cff3c34ce5d804d579eb6a7114e86b3b6b
Susannah <susannah.tysor@gmail.com>
2020-05-31 08:32:16 -0700

    Rename file and build a list of TRY accession numbers
    for Toronto species

--- scripts/matchGbifTry.R
+++ scripts/matchGbifTry.R
@@ -6,21 +6,21 @@ library(tidyverse)
 vanSpecies <- read.csv("data/VancouverSpeciesList.csv")
 edmSpecies <- read.csv("data/EdmontonSpeciesList.csv")
 winSpecies <- read.csv("data/WinnipegSpeciesList.csv")
-torSepcies <- read.csv("data/TorontoSpeciesList.csv")
+torSpecies <- read.csv("data/TorontoSpeciesList.csv")
 mtlSpecies <- read.csv("data/MontrealSpeciesList.csv")
 halSpecies <- read.csv("data/HalifaxSpeciesList.csv")
 
 # grab list of all the species in TRY
 trySpecies <- read.csv("data/TRY_specieslist.csv") %>%
-    unite("scientificName", Genus, Species, sep=" ") # genus and species are separate columns in try data, one in the gbif species list
+    unite("scientificName", Genus, Species, sep=" ") # put genus and species in one column
 
 # Which species in our quadrats are in the TRY database?
 
 # scientific names from gbif contain the authority, but in try they do not.
-unique(speciesList$scientificName)[1:100]
-trySpecies$scientificName[1:100]
+unique(speciesList$scientificName)[1:10]
+trySpecies$scientificName[1:10]
 
-# TODO strip authority from gbif
+# strip authority from gbif
 vanSpecies[, "scientificName"] <- gbif_parse(vanSpecies$scientificName)[, "canonicalname"]
 edmSpecies[, "scientificName"] <- gbif_parse(edmSpecies$scientificName)[, "canonicalname"]
 winSpecies[, "scientificName"] <- gbif_parse(winSpecies$scientificName)[, "canonicalname"]
@@ -31,6 +31,10 @@ halSpecies[, "scientificName"] <- gbif_parse(halSpecies$scientificName)[, "canon
 
 # get list of accession numbers to paste into TRY https://www.try-db.org/TryWeb/Prop2.php
 
-trySpecies[trySpecies$scientificName %in% speciesList$scientificName,]$AccSpeciesID
+torOverlap <- trySpecies[trySpecies$scientificName %in% torSpecies$scientificName,] # TRY species in Toronto species list
 
+write.table(x=torOverlap$AccSpeciesID, "data/tryAccession/tor_accession.csv", row.names = FALSE, col.names = FALSE, sep=",")
+
+# Proportion of Toronto species NOT included in TRY
+(nrow(torSpecies) - nrow(torOverlap))/nrow(torSpecies) # ~47%
 
