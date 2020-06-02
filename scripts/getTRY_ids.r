ba71f4848f92c9cb14f24cdd3bfd0a988331ddee
Susannah <susannah.tysor@gmail.com>
2020-06-01 19:41:52 -0700

    refactor matching and accession code
    - plus change to only providing unique accession codes
    - write out accessionno file

--- scripts/getTRY_ids.r
+++ scripts/getTRY_ids.r
@@ -1,5 +1,5 @@
-# Script to winnow down the species to 11 occurence min and then matching with TRYspeciesList to get the accessNumbers.
-library(dplyr)
+# Match GBIF master species list with TRYspeciesList to get the access ids.
+
 # Reading in the masterSpeciesList of each city that got cleaned in OpenRefine, and the TRYSpeciesList
 MasterSpeciesList <- read.csv("data/MasterSpeciesList_clean.csv", header = TRUE, stringsAsFactors = FALSE)
 TRY_SpeciesList <- read.csv("data/TRY_specieslist.csv", header = TRUE, stringsAsFactors = FALSE)
@@ -7,23 +7,26 @@ TRY_SpeciesList <- read.csv("data/TRY_specieslist.csv", header = TRUE, stringsAs
 # Make one colum with Genus + Species in the TRY list
 TRY_SpeciesList[,"species"] <- paste(TRY_SpeciesList$Genus, TRY_SpeciesList$Species, sep = " ")
 
-# Onyl get the needed info
+# Only get the needed info
 TRY_SpeciesList <- TRY_SpeciesList[,c("AccSpeciesID","species")]
 
-# Join the two file based on the species name
-SpeciesList_joined <- dplyr::left_join(MasterSpeciesList, TRY_SpeciesList, by = "species")
-SpeciesList_joined <- unique(SpeciesList_joined[,c("species", "AccSpeciesID")]) # Get only the names and the TRY IDs
-SpeciesList_joined <- SpeciesList_joined[which(!is.na(SpeciesList_joined$AccSpeciesID)),] # Remove the row that has ID = NA
-AccSpeciesID <- SpeciesList_joined[,"AccSpeciesID"] # Get only the IDs as a vector
+# Drop NAs from gbif species list
+MasterSpeciesList <- MasterSpeciesList[which(!is.na(MasterSpeciesList$species)),]
 
-# Separate the IDs in chunck of 1000 for the retrieval on TRYdatabase
-# (Susannah's code)
+# Join the two file based on the species name
+SpeciesList_joined <- TRY_SpeciesList[TRY_SpeciesList$species %in% MasterSpeciesList$species,]
+AccSpeciesID <- unique(SpeciesList_joined[,"AccSpeciesID"]) # Get only the IDs as a vector
+
+# Sanity check
+length(AccSpeciesID) < nrow(TRY_SpeciesList)
+
+# Separate the IDs in chunk of 1000 for the retrieval on TRYdatabase
 # write out accession numbers for pasting into TRY
 ## TRY will only accept 1000 species codes at a time, so write out accession numbers file with lines of 1000 species codes each. Make sure to open in a text editor and not a spreadsheet program for proper copy-pasting.
 
-breaks <- seq(from=1, to=length(AccSpeciesID), by=1000)
 if (file.exists("data/tryAccession/accessionMaster.csv")) file.remove("data/tryAccession/accessionMaster.csv") # the for loop below will just keep appending to the existing file - this line prevents duplication within the file
 
+breaks <- seq(from=1, to=length(AccSpeciesID), by=1000)
 for (i in 1:length(breaks)) {
     accessionNos <- AccSpeciesID[breaks[i]:(breaks[i] + 999)]
     write.table(t(accessionNos), "data/tryAccession/accessionMaster.csv", col.names=FALSE, row.names = FALSE, sep=",", append=TRUE)
