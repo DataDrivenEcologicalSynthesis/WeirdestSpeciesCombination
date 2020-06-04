8818ed24c189f9aba78e57f6788024ae2afadb77
Susannah <susannah.tysor@gmail.com>
2020-06-04 13:58:54 -0700

    most normal and most extreme lists

--- scripts/speciescombinations.r
+++ scripts/speciescombinations.r
@@ -9,15 +9,25 @@ bycity <- species %>%
     map(select, species)
 
 #all cities
-inallcities <- Reduce(intersect, bycity) #species that occur in all cities - the most common city community
+inallcities <- Reduce(intersect, bycity) #species that occur in all cities - the most common city community. 55 species total
 
-# what % of the total species in a city is the most common species list
+# what % of the total species in a city is in the most common species list
 species %>%
     select(-quadrat) %>%
     distinct() %>%
     group_by(City) %>%
     summarize(percent_common = (nrow(inallcities)/n())*100) %>%
     arrange(desc(percent_common))
+
+# Percent of a city's species that are in the set of species that occur in all cities
+# City      percent_common
+# <chr>              <dbl>
+# 1 Winnipeg           16.4
+# 2 Edmonton            9.15
+# 3 Halifax             5.11
+# 4 Toronto             4.31
+# 5 Montreal            3.56
+# 6 Vancouver           2.10
 
 
 # all species that occur in more than one quadrat
