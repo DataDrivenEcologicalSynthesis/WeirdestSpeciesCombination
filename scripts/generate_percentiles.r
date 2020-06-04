cdc05796078ac2ff36a1afea0fa70239470c4365
Benjamin <benjamin.b.mercier@usherbrooke.ca>
2020-06-04 11:20:03 -0400

    Modified scripts figure

--- scripts/generate_percentiles.r
+++ scripts/generate_percentiles.r
@@ -130,14 +130,18 @@ write.csv(hyperlocal_weird, "analysis/quadrat_weird.csv", row.names = FALSE)
 test <- sla %>%
     select(-TraitName, -quadrat) %>%
     group_by(City)
-cityHist <- ggplot(test, aes(x = value, fill =  as.factor(City))) +
-    facet_wrap(~ City) +
-    geom_histogram(position = "stack", bins = 50) +
-    ggtitle("Distribution of SLA by city, 5 quadrats confound")
 
-cityHist + theme(legend.title = element_blank()) +
+cityHist <- ggplot(test, aes(x = value, fill =  City)) +
+    facet_wrap(~ City) +
+    geom_histogram(position = "stack", bins = 50)
+    
+
+cityHist <- cityHist + theme(legend.position = "None") +
     labs(y = "Count",
          x = expression(paste("Specific Leaf Area (mm"^2, " ", mg^-1, sep=")")))
+
+ggsave("figures/cityHist.png", plot=cityHist, width=16, height=9, units="in")
+
 
 # Trying to make the 5 quadrat for each city (6x5 plots)
 test <- sla %>%
@@ -164,9 +168,10 @@ sampEff <- sla %>%
     count()
 
 sampEff_plot <- ggplot(sampEff, aes(x = City, y = n, fill = City)) +
-    geom_bar(stat = "identity") +
-    ggtitle("Number of observation per city, all species confound")
+    geom_bar(stat = "identity")
 
-sampEff_plot + theme(legend.title = element_text("City")) +
-    labs(y = "Number of observation",
-         x = "City")
+sampEff_plot <- sampEff_plot + theme_classic(base_size = 18) +
+    theme(legend.position = "none") +
+    labs(x = " ",
+         y = "Number of species")  
+ggsave("figures/sampling_spfig.png", plot=sampEff_plot, width=16, height=9, units="in")
